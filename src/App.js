9aef7c19b41041328fd0357a9bdafda2501da1c1
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-02-03 21:53:42 -0300

    New adjustment made to the App.js file to the new standard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-// Import dependencies
 import React, { useRef, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./App.css";
-import { desenhoRect } from "./utilitarios"
+import { drawRect } from "./utilities";
 
 function App() {
-  const titulo = document.querySelector('h1');
-  escritaTitulo(titulo);
+  const title = document.querySelector('h1');
+  typeWriterEffect(title);
 
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
@@ -22,7 +20,6 @@ function App() {
   };
 
   const detect = async (net) => {
-
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
@@ -38,12 +35,12 @@ function App() {
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      const obj = await net.detect(video);
-      console.log(obj);
+      const objects = await net.detect(video);
+      console.log(objects);
 
       const ctx = canvasRef.current.getContext("2d");
 
-      desenhoRect(obj, ctx);
+      drawRect(objects, ctx);
     }
   };
 
@@ -51,30 +48,29 @@ function App() {
 
   return (
     <div>
-        <Webcam
-          ref={webcamRef}
-          muted={true}
-          style={{
-            borderRadius: 10
-          }}
-        />
+      <Webcam
+        ref={webcamRef}
+        muted={true}
+        style={{
+          borderRadius: 10
+        }}
+      />
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            right: 45
-          }}
-        />
+      <canvas
+        ref={canvasRef}
+        style={{
+          right: 45
+        }}
+      />
     </div>
   );
 }
 
-
-function escritaTitulo(elemento) {
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
-  for (let i = 0; i < textoArray.length; i++) {
-    setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+function typeWriterEffect(element) {
+  const textArray = element.innerHTML.split('');
+  element.innerHTML = '';
+  for (let i = 0; i < textArray.length; i++) {
+    setTimeout(() => element.innerHTML += textArray[i], 75 * i);
   }
 }
 
